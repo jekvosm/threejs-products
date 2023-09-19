bd2c068d353489bfa49c3271b1af08841fa2e965
jekvosm <yn4k-jek@yandex.ru>
2023-09-19 22:23:37 +0400

    created custom button, some empty components, add tabs to the Cusomizer component

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from './custom-button.component'
+import AIPicker from './ai-picker.component'
+import ColorPicker from './color-picker.component'
+import FilePicker from './file-picker.component'
+import Tab from './tab.component'
+
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab }
